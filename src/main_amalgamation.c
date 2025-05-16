eec887588d7c6db34985092e5f0713db54eb75ea
VitorJuan <cosmos@unochapeco.edu.br>
2025-05-16 19:14:34 -0300

    Atualizando o repositório

--- src/main_amalgamation.c
+++ src/main_amalgamation.c
@@ -10,9 +10,15 @@ Projeto que cria um sistema para poder criar, armazenar, comprar e gerenciar pro
 
 /*
 TODO:
-[ ] Produzir o código da função sbPushString
-[ ] Produzir uma arquitetura para o banco de dados, talvez CSV, ou implementar leitura de arquivos JSON em C, ou utilizar de sqlit
+[X] Produzir o código da função sbPushString
 [ ] Fazer a verificação de dados, pois existe a possibilidade de que ao coletar algum dado quando registrando algum produto ocorra um erro pois o ciente não está digitando o tipo de dados corretos, ou digitou algum caractere errado
+[ ] Implementar um sistema de non-case-sensetive, permetindo que ao usuario digitar alguma informação e ao processa-la não irá existir problemas em caso de letras em modo case-sensitive
+[ ] Verificar o problema de casting de string pra C quando lendo um input do usuario, esse problema ocorre na função de nome createProduct na seção PRODUCT HANDLE SECTION
+[ ] Implementar todas as função faltantes na seção de Products Handle
+[ ] implementar algumas caracteristicas mais complexas no banco de dados como sistema de quantidade do mesmo produto, a soma total dos valores desse produto
+
+TODO EVERITIME:
+[ ] Verificar a necessidade de retirar alguma seção que não será mais usada no código
 */
 
 /*
@@ -40,13 +46,14 @@ typedef struct {
     int index;
     int size;
 }Product_Database;
+Product_Database product_database = {0};
 
 //STRING BUILDER DECLARATION
 void sbInit(StringBuilder* str, int initialCapacity);
 void sbPushChar(StringBuilder* str, char ch);
 void sbResize(StringBuilder* str);
 char* sbGetContent(StringBuilder* str);
-void sbPushString(StringBuilder* str, const char* input);
+void sbPushString(StringBuilder* str, char* input);
 void sbFree(StringBuilder* str);
 
 //TERMINAL INTERFACE DECLARATION
@@ -55,10 +62,11 @@ void processUserInput(char* userInput);
 void help_prompt();
 
 //PRODUCT DATABASE HANDLE DECLARATION
-void pdDbInit(Product_Database* pd_db, int initialCapacity);
+void pdDbInit(Product_Database* pd_db, int  initialCapacity);
 void pdDbResize(Product_Database* pd_db);
 void pdDbInsertProduct(Product_Database* pd_db, Product* product);
 Product pdDbGetProduct(Product_Database* pd_db, int index);
+Product pdDbGetProductByName(Product_Database* pd_db, char* product_name);
 void pdDbFree(Product_Database* pd_db);
 
 //PRODUCT DECLARATION
@@ -91,7 +99,7 @@ char* readFile(FILE* file) {
 
 /*
 ------------------------------------------------------------------------------------------------
-                                PRODUCT DATBASE HANDLES CODE SECTION
+                                PRODUCT DATABASE HANDLES CODE SECTION
 ------------------------------------------------------------------------------------------------
 */
 
@@ -128,6 +136,36 @@ Product pdDbGetProduct(Product_Database* pd_db, int index) {
     return pd_db->product[index];
 }
 
+Product pdDbGetProductByName(Product_Database* pd_db, char* product_name) {
+    for (int x = 0; x < pd_db->size; x++) {
+        if (strcmp(product_name, pd_db->product[x].name) == 0) {
+            return pd_db->product[x];
+        }
+    }
+}
+
+void pdDbDeleteProduct(Product_Database* pd_db, int index) {
+    int found = 0;
+    
+    for (int x = 0; x < pd_db->index; x++) {
+        if(pd_db->product[x] == index) {
+            found = 1;
+        }
+        if (found && x < list->size - 1) {
+            pd_db->product[x] = pd_db->product[x + 1];
+        }
+    }
+    
+    if (found) {
+        pd_db->index--;
+    }
+}
+
+//Essa função é responsável por lidar com a opção de compra do produto, isto é, diminuir a quantia de produtos disponíveis, isto é, realizar uma baixa no produto, e também aumentar diminuir o valor (quantia total de dinheiro em mercadoria) da quantidade de itens na base de dados 
+void pdDbPurchaseProduct() {
+    
+}
+
 void pdDbFree(Product_Database* pd_db) {
     free(pd_db->product);
     pd_db->index = 0;
@@ -143,6 +181,8 @@ void pdDbFree(Product_Database* pd_db) {
 void createProduct() {
     printf("Iniciando o sistema para poder criar um produto\n\n");
 
+    Product product = {0};
+
     StringBuilder product_name;
     StringBuilder product_price;
     StringBuilder product_kg;
@@ -153,17 +193,33 @@ void createProduct() {
 
     printf("Digite o nome do produto\n");
     readUserInput(&product_name);
-    
+    product.name = sbGetContent(&product_name);
+
     printf("Digite o preço do produto\n");
     readUserInput(&product_price);
+    product.price = (int) sbGetContent(&product_price);
 
     printf("Digite o kg do produto\n");
     readUserInput(&product_kg);
+    product.kg = (int) sbGetContent(&product_kg);
+
+    pdDbInsertProduct(&product_database, &product);
 
     printf("As seguintes informações foram registradas\n");
     printf("NOME DO PRODUTO: %s\n", sbGetContent(&product_name));
     printf("PREÇO DO PRODUTO: %s\n", sbGetContent(&product_price));
     printf("KG DO PRODUTO: %s\n", sbGetContent(&product_kg));
+
+    /*
+    StringBuilder user_input;
+    sbInit(&user_input, 10);
+    printf("\nVocê deseja adicionar um novo produto? Y/N\n");
+
+    readUserInput(&user_input);
+    if(strcmp("Y", sbGetContent(&user_input)) == 0) {
+        createProduct();
+    }
+    */
 
     sbFree(&product_name);
     sbFree(&product_price);
@@ -172,6 +228,21 @@ void createProduct() {
 }
 
 void deleteProduct() {
+    StringBuilder user_input = {0};
+
+    sbInit(&user_input);
+    
+    printf("Por favor, digite o nome do produto a ser deletado\n");
+    readUserInput(&user_input);
+
+    printf("Buscando o produto na base de dados\n");
+    for (int x = 0; x < pd_db->index; x++) {
+        
+    }
+
+}
+
+void deleteProductByName() {
     
 }
 
@@ -179,8 +250,24 @@ void buyProduct() {
 
 }
 
+void buyProductByName() {
+
+}
+
 void editProduct() {
 
+}
+
+void editProductByName() {
+
+}
+
+void listAllProducts(Product_Database* pd_db) {
+    for (int x = 0; x < pd_db->index; x++) {
+        printf("Product Name: %s\n", pd_db->product[x].name);
+        printf("Product Price: %s\n", pd_db->product[x].price);
+        printf("Product KG: %s\n", pd_db->product[x].kg);
+    }
 }
 
 /*
@@ -220,6 +307,11 @@ void processUserInput(char* userInput) {
         buyProduct();
     }
 
+    else if (strcmp(userInput, "list_products") == 0) {
+        printf("Listando todos os produtos cadastrados no sistema e suas informações\n");
+        listAllProducts(&product_database);
+    }
+
     else {
         printf("comando \"%s\" nao encontrado, digite \"help\" para uma lista de comandos\n", userInput);
     }
@@ -231,6 +323,7 @@ void help_prompt() {
     printf("create_product - incializa o sistema para criar um produto\n");
     printf("delete_product - incializa o sistema para poder deleter um produto\n");
     printf("buy_product    - simula a compra de um produto por um cliente\n");
+    printf("list_products  - Informa todos os produtos cadastrados e suas informações\n");
 }
 
 /*
@@ -262,8 +355,13 @@ char* sbGetContent(StringBuilder* str) {
     return str->array;
 }
 
-void sbPushString(StringBuilder* str, const char* input) {
+void sbPushString(StringBuilder* str, char* input) {
     //T0D0: Pegar o string inpit, coletar a quantia de caracteres e colocar os caracteres 1 a 1 usando a função pushChar()
+    int string_size = strlen(input);
+    for (int x = 0; x < string_size; x++) {
+        sbPushChar(str, input[x]);
+    }
+
 }
 
 void sbFree(StringBuilder* str) {
@@ -288,6 +386,8 @@ void readDatabase() {
 
 
 int main () {
+    pdDbInit(&product_database, 1024);
+
     printf("Sistema de gerenciamento de produtos via terminal. Digite help para listar comandos\n");
 
     StringBuilder userInput = {0};
